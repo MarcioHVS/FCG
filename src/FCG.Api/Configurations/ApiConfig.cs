18c02b27f609c6919753f1c385b0d46109f23211
Marcio Henrique <marciohenriquev@gmail.com>
2025-05-01 15:22:56 -0300

    Refatorando a classe Program

---
+++ src/FCG.Api/Configurations/ApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using FCG.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace FCG.Api.Configurations
+{
+    public static class ApiConfig
+    {
+        public static WebApplicationBuilder AddApiConfiguration(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddAuthorization();
+            builder.Services.AddControllers();
+
+            return builder;
+        }
+
+        public static WebApplication UseApiConfiguration(this WebApplication app)
+        {
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            return app;
+        }
+    }
+}
