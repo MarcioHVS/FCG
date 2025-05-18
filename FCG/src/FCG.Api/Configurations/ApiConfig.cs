2e4a23c76ea23d562fac0dc0e07cac6da8668416
Marcio Henrique <marciohenriquev@gmail.com>
2025-05-17 22:23:05 -0300

    Implementação inicial da camada Api

---
+++ FCG/src/FCG.Api/Configurations/ApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using FCG.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
+namespace FCG.Api.Configurations
+{
+    public static class ApiConfig
+    {
+        public static WebApplicationBuilder AddApiConfiguration(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddAuthorization();
+            builder.Services.AddControllers()
+                .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+            return builder;
+        }
+
+        public static WebApplication UseApiConfiguration(this WebApplication app)
+        {
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            return app;
+        }
+    }
+}
